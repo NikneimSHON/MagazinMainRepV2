1a94c61ec41f5c53d05ed0642b3c73f6a464396c
tsarp <tsarpkin.nmk22@yadenx.ru>
2025-04-08 13:04:47 +0300

    Spring#2

--- service/src/main/java/com/nikita/shop/database/repository/RepositoryBase.java
+++ service/src/main/java/com/nikita/shop/database/repository/RepositoryBase.java
@@ -2,6 +2,7 @@ package com.nikita.shop.database.repository;
 
 import com.nikita.shop.database.entity.BaseEntity;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaQuery;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,33 +19,43 @@ public abstract class RepositoryBase<K extends Serializable, E extends BaseEntit
     private final Class<E> entityClass;
 
     @Override
+    @Transactional
     public E save(E entity) {
-        entityManager.persist(entity);
-        return entity;
+        if (entity.getId() == null) {
+            entityManager.persist(entity);
+            return entity;
+        }
+        return entityManager.merge(entity);
     }
 
     @Override
+    @Transactional
     public void delete(E entity) {
-        entityManager.remove(entity);
-        entityManager.flush();
+        entityManager.remove(
+                entityManager.contains(entity) ? entity : entityManager.merge(entity)
+        );
     }
 
     @Override
+    @Transactional
     public void update(E entity) {
         entityManager.merge(entity);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<E> findById(K id) {
         return Optional.ofNullable(entityManager.find(entityClass, id));
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<E> findAll() {
-        var cb = entityManager.getCriteriaBuilder();
-        var criteria = cb.createQuery(entityClass);
-        criteria.from(entityClass);
+        CriteriaQuery<E> criteria = entityManager.getCriteriaBuilder()
+                .createQuery(entityClass);
+        criteria.select(criteria.from(entityClass));
         return entityManager.createQuery(criteria)
                 .getResultList();
     }
+
 }
